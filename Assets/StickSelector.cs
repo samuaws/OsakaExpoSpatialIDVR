73c29e1d465bd1bc7b4c9600708b851b6406903b
Sami Brahim Djelloul <61477348+samuaws@users.noreply.github.com>
2025-09-24 21:59:57 +0900

    saving to database

--- Assets/StickSelector.cs
+++ Assets/StickSelector.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Collections;
 
 public class StickSelector : MonoBehaviour
 {
@@ -16,6 +18,10 @@ public class StickSelector : MonoBehaviour
     public Material transparentMaterial;
     public Material selectableMaterial;
     public Material selectedMaterial;
+
+    [Header("API Settings")]
+    public string apiBaseUrl = "http://localhost:5000/api/attributes/";
+    public int zoomLevel = 25;
 
     // Track overlapping cells
     private List<Collider> overlappingCells = new List<Collider>();
@@ -55,6 +61,12 @@ public class StickSelector : MonoBehaviour
 
                 Debug.Log("Cell selected: " + currentHoverCell.gameObject.name);
             }
+        }
+
+        // Left controller button -> send to API
+        if (OVRInput.GetDown(OVRInput.Button.Three)) // "X" button on left controller
+        {
+            SaveSelectedCells();
         }
     }
 
@@ -103,6 +115,89 @@ public class StickSelector : MonoBehaviour
             }
 
             UpdateHoverCell();
+        }
+    }
+
+    private void SaveSelectedCells()
+    {
+        Debug.Log("Saving selected cells to API...");
+        foreach (GameObject cell in new List<GameObject>(selectedCells)) // copy to avoid modifying list while iterating
+        {
+            SpatialIdCell idCell = cell.GetComponent<SpatialIdCell>();
+            if (idCell != null)
+            {
+                string spatialId = idCell.spatialId;
+                StartCoroutine(SendPostRequest(spatialId, cell));
+            }
+        }
+    }
+
+    private IEnumerator SendPostRequest(string spatialId, GameObject cell)
+    {
+        string url = apiBaseUrl + spatialId;
+        Debug.Log("Posting to " + url);
+
+        // Example attributes you want to save (customize this!)
+        var payload = new
+        {
+            zoom_level = zoomLevel,
+            attributes = new Dictionary<string, object>
+            {
+                { "selected", true }
+            }
+        };
+
+        string jsonData = JsonUtility.ToJson(new Wrapper(payload));
+
+        using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Successfully saved SpatialID " + spatialId);
+                selectedCells.Remove(cell); // remove from temp list -> stays visually selected
+            }
+            else
+            {
+                Debug.LogError("Error saving SpatialID " + spatialId + ": " + request.error);
+            }
+        }
+    }
+
+    // Helper to wrap dictionary since JsonUtility does not support it directly
+    [System.Serializable]
+    private class Wrapper
+    {
+        public int zoom_level;
+        public SerializableDict attributes;
+
+        public Wrapper(object data)
+        {
+            var dict = (Dictionary<string, object>)((dynamic)data).attributes;
+            zoom_level = ((dynamic)data).zoom_level;
+            attributes = new SerializableDict(dict);
+        }
+    }
+
+    [System.Serializable]
+    private class SerializableDict
+    {
+        public List<string> keys = new List<string>();
+        public List<string> values = new List<string>();
+
+        public SerializableDict(Dictionary<string, object> dict)
+        {
+            foreach (var kvp in dict)
+            {
+                keys.Add(kvp.Key);
+                values.Add(kvp.Value.ToString());
+            }
         }
     }
 }
